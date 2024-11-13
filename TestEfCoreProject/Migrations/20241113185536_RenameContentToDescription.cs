9b54e2332909f09adc9426c3a72e7bc401f947dc
Jason Gallavin <4037224+jasekiw@users.noreply.github.com>
2024-11-13 13:56:37 -0500

    add migrations to rename and then delete the column

---
+++ TestEfCoreProject/Migrations/20241113185536_RenameContentToDescription.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TestEfCoreProject.Migrations
+{
+    /// <inheritdoc />
+    public partial class RenameContentToDescription : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Content",
+                table: "Posts",
+                newName: "Description");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Description",
+                table: "Posts",
+                newName: "Content");
+        }
+    }
+}
---
+++ TestEfCoreProject/Migrations/20241113185550_DeleteDescription.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TestEfCoreProject.Migrations
+{
+    /// <inheritdoc />
+    public partial class DeleteDescription : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Description",
+                table: "Posts");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Description",
+                table: "Posts",
+                type: "longtext",
+                nullable: false)
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+    }
+}
